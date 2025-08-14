101e784818906f0ef02cf94fa08969d3b612be92
shubh30 <shubhamnagpal30@gmail.com>
2025-08-14 17:05:44 +0530

    Basic styles

---
+++ src/App.style.ts
@@ -0,0 +1,19 @@
+import { css } from "@emotion/react";
+
+export const containerStyle = css`
+  padding: 20px;
+  min-height: 100vh;
+`
+
+export const headingStyle = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`
+
+export const titleStyle = css`
+  font-size: 32px;
+  color: #333;
+  margin: 0;
+`
---
+++ src/components/Home/Home.style.ts
@@ -0,0 +1,23 @@
+import { css } from "@emotion/react";
+
+export const listStyle = css`
+  color: #000;
+  padding: 10px;
+`
+
+export const itemStyle = css`
+  cursor: pointer;
+`
+
+export const titleStyle = css`
+  font-size: 24px;
+  margin-bottom: 10px;
+`
+
+export const subtitleStyle = css`
+  font-size: 18px;
+  margin-bottom: 20px;
+  color: #555;
+  font-weight: 400;
+  line-height: 1.5;
+`
